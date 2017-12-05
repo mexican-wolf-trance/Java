8b8b95a6fcfa200f2275e18c477065bb572b79b2
Mexican Wolf <charlestwyatt@live.com>
2017-12-05 03:55:43 -0600

    changed to check if out of bounds at each if/else statment

--- ChainCode/src/ChainCode/ChainCode.java
+++ ChainCode/src/ChainCode/ChainCode.java
@@ -39,10 +39,10 @@ public class ChainCode
 		//System.out.println("chain.length = " + chain.length + "\nchain[0].length = " + chain[0].length);
 		for(row = 0; row < chain.length; row++)
 		{
-			//System.out.println("ROW: " + row);
+			System.out.println("ROW: " + row);
 			for(col = 0; col < chain[0].length; col++)
 			{
-				//System.out.println("col: " + col);
+				System.out.println("col: " + col);
 				if (chain[row][col] == 1)
 				{
 					startRow = row;
@@ -51,98 +51,110 @@ public class ChainCode
 					
 					while(true)
 					{
-						//System.out.println("WHILE LOOP");
+						System.out.println("WHILE LOOP");
 						//DOWN
 						if (chain[startRow + 1][startCol] == 1)
 						{
-							//System.out.println("startRow: " + startRow + "\nstartCol: " + startCol);
+							System.out.println("startRow: " + startRow + "\nstartCol: " + startCol);
 							chain[startRow][startCol] = 0;
-							//System.out.println("GOTCHA");
+							System.out.println("GOTCHA");
 							comp[i] = 6;
-							//System.out.println("comp" + i +": " + comp[i]);
+							System.out.println("comp" + i +": " + comp[i]);
 							i++;
-							startRow += 1;
+							if ((startRow + 1) != chain.length)
+								startRow += 1;
 						}
 						//RIGHT
 						else if (chain[startRow][startCol + 1] == 1)
 						{
-							//System.out.println("startRow: " + startRow + "\nstartCol: " + startCol);
+							System.out.println("startRow: " + startRow + "\nstartCol: " + startCol);
 							chain[startRow][startCol] = 0;
-							//System.out.println("GOTCHA");
+							System.out.println("GOTCHA");
 							comp[i] = 0;
-							//System.out.println("comp" + i +": " + comp[i]);
+							System.out.println("comp" + i +": " + comp[i]);
 							i++;
-							startCol += 1;
+							if ((startCol + 1) != chain[0].length)
+								startCol += 1;
 						}
 						//LEFT
 						else if (chain[startRow][startCol - 1] == 1)
 						{
-							//System.out.println("startRow: " + startRow + "\nstartCol: " + startCol);
+							System.out.println("startRow: " + startRow + "\nstartCol: " + startCol);
 							chain[startRow][startCol] = 0;
-							//System.out.println("GOTCHA");
+							System.out.println("GOTCHA");
 							comp[i] = 4;
-							//System.out.println("comp" + i +": " + comp[i]);
+							System.out.println("comp" + i +": " + comp[i]);
 							i++;
-							startCol -= 1;
+							if ((startCol - 1) >= 0)
+								startCol -= 1;
 						}
 						//TOP
 						else if (chain[startRow - 1][startCol] == 1)
 						{
-							//System.out.println("startRow: " + startRow + "\nstartCol: " + startCol);
+							System.out.println("startRow: " + startRow + "\nstartCol: " + startCol);
 							chain[startRow][startCol] = 0;
-							//System.out.println("GOTCHA");
+							System.out.println("GOTCHA");
 							comp[i] = 2;
-							//System.out.println("comp" + i +": " + comp[i]);
+							System.out.println("comp" + i +": " + comp[i]);
 							i++;
-							startRow -= 1;
+							if ((startRow - 1) >= 0)
+								startRow -= 1;
 						}
 						//DOWN RIGHT
 						else if (chain[startRow + 1][startCol + 1] == 1)
 						{
-							//System.out.println("startRow: " + startRow + "\nstartCol: " + startCol);
+							System.out.println("startRow: " + startRow + "\nstartCol: " + startCol);
 							chain[startRow][startCol] = 0;
-							//System.out.println("GOTCHA");
-							//System.out.println("comp" + i +": " + comp[i]);
+							System.out.println("GOTCHA");
+							System.out.println("comp" + i +": " + comp[i]);
 							comp[i] = 7;
 							i++;
-							startRow += 1;
-							startCol +=1;
+							if ((startRow + 1) != chain.length)
+								startRow += 1;
+							if ((startRow + 1) != chain[0].length)
+								startCol +=1;
 						}
 						//DOWN LEFT
 						else if (chain[startRow + 1][startCol - 1] == 1)
 						{
-							//System.out.println("startRow: " + startRow + "\nstartCol: " + startCol);
+							System.out.println("startRow: " + startRow + "\nstartCol: " + startCol);
 							chain[startRow][startCol] = 0;
-							//System.out.println("GOTCHA");
+							System.out.println("GOTCHA");
 							comp[i] = 5;
-							//System.out.println("comp" + i +": " + comp[i]);
+							System.out.println("comp" + i +": " + comp[i]);
 							i++;
-							startRow += 1;
-							startCol -= 1;
+							if ((startRow + 1) != chain.length)
+								startRow += 1;
+							if ((startCol - 1) >= 0)
+								startCol -= 1;
 						}
 						//TOP LEFT
 						else if (chain[startRow - 1][startCol - 1] == 1)
 						{
-							//System.out.println("startRow: " + startRow + "\nstartCol: " + startCol);
+							System.out.println("startRow: " + startRow + "\nstartCol: " + startCol);
 							chain[startRow][startCol] = 0;
-							//System.out.println("GOTCHA");
+							System.out.println("GOTCHA");
 							comp[i] = 3;
-							//System.out.println("comp" + i +": " + comp[i]);
+							System.out.println("comp" + i +": " + comp[i]);
 							i++;
-							startRow -= 1;
-							startCol -= 1;
+							if ((startRow - 1) >= 0)
+								startRow -= 1;
+							if ((startCol - 1) >= 0)
+								startCol -= 1;
 						}
 						//TOP RIGHT
 						else if (chain[startRow - 1][startCol + 1] == 1)
 						{
-							//System.out.println("startRow: " + startRow + "\nstartCol: " + startCol);
+							System.out.println("startRow: " + startRow + "\nstartCol: " + startCol);
 							chain[startRow][startCol] = 0;
-							//System.out.println("GOTCHA");
+							System.out.println("GOTCHA");
 							comp[i] = 1;
-							//System.out.println("comp" + i +": " + comp[i]);
+							System.out.println("comp" + i +": " + comp[i]);
 							i++;
-							startRow -= 1;
-							startCol += 1;
+							if ((startRow - 1) >= 0)
+								startRow -= 1;
+							if ((startCol + 1) != chain[0].length)
+								startCol += 1;
 						}
 						else
 						{
@@ -156,7 +168,7 @@ public class ChainCode
 							break;
 						}
 						
-						/if ((startRow + 1) == (chain.length) || (startCol + 1) == (chain[0].length) || (startRow - 1) < 0 || (startCol - 1) < 0)
+						/*if ((startRow + 1) == (chain.length) || (startCol + 1) == (chain[0].length) || (startRow - 1) < 0 || (startCol - 1) < 0)
 						{
 							chain[startRow][startCol] = 0;
 							check[i] = 10;
@@ -166,7 +178,7 @@ public class ChainCode
 							comp = new int[20];
 							i = 0;
 							break;
-						}
+						}*/
 					}
 				}
 			}
