67a9d6d1fb1f9872aed6d6b60e210c98144dc0ff
Mexican Wolf <charlestwyatt@live.com>
2017-12-04 19:41:01 -0600

    new java project: declare mult-dim array

---
+++ ChainCode/src/ChainCode/ChainCode.java
@@ -0,0 +1,15 @@
+package ChainCode;
+
+public class ChainCode 
+{
+
+	public static void main(String[] args) 
+	{
+		int col = 8, row = 8;
+		double[][] chain = new double[col][row];
+		
+		
+
+	}
+
+}
