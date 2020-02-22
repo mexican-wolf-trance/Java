37e98846dd2becfff0d27a287c1abd92f8cf4a93
mexican-wolf-trance <bargain.ghost@yandex.com>
2020-02-22 14:27:11 -0600

    creating new project

---
+++ randomCityPathGenerator/src/PathFinder.java
@@ -0,0 +1,15 @@
+package randomCityPathGenerator;
+import java.util.*;
+
+public class PathFinder
+{
+    public static void main (String[] args)
+    {
+        int k = 0;
+        Scanner cityInput = new Scanner(System.in);
+        System.out.print("Enter the number of cities");
+        k = cityInput.nextInt();
+
+        System.out.print(k);
+    }
+}
