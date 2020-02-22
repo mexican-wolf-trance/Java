3cc7e8de97a304e057895f666c75d12d088f60e4
mexican-wolf-trance <bargain.ghost@yandex.com>
2020-02-22 16:15:17 -0600

    adding second input for the grid length

--- randomCityPathGenerator/src/PathFinder.java
+++ randomCityPathGenerator/src/PathFinder.java
@@ -6,6 +6,7 @@ public class PathFinder
     public static void main (final String[] args)
     {
         int k = 0;
+        int n = 0;
         final Scanner cityInput = new Scanner(System.in);
         while(true)
         {
@@ -31,7 +32,32 @@ public class PathFinder
                 continue;
             }
         }
-        System.out.print(k + " cities");
+        while(true)
+        {
+            try
+            {
+                System.out.println("Enter the length of one side of the square grid between 10 and 30:");            
+                n = cityInput.nextInt();
+                if(n >= 10 && n <= 30)
+                    break;
+                else
+                {
+                    System.out.println("That is an integer, but it must be between 10 and 30");
+                    cityInput.next();
+                    System.out.println();
+                    continue;
+                }
+            }
+            catch(InputMismatchException e)
+            {
+                System.out.println("Must be an integer between 10 and 30");
+                cityInput.next();
+                System.out.println();
+                continue;
+            }
+        }
         cityInput.close();
+        System.out.println(k + " cities");
+        System.out.println(n + "length");
     }
 }
