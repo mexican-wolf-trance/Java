e1c2a5c42429d1d88fbc9ffc8db83bb74c7deaa5
mexican-wolf-trance <bargain.ghost@yandex.com>
2020-02-24 19:00:17 -0600

    complete overhaul of the input validation

--- randomCityPathGenerator/src/PathFinder.java
+++ randomCityPathGenerator/src/PathFinder.java
@@ -10,55 +10,29 @@ public class PathFinder
         final Scanner cityInput = new Scanner(System.in);
 
         System.out.println("Enter the number of cities between 4 and 9:");            
-        k = cityInput.nextInt();
 
-        while(true)
-        {
-            try
-            {
-                if(k >= 4 && k <= 9)
-                    break;
-                else
-                {
-                    System.out.println("That is an integer, but it must be between 4 and 9");
-                    //cityInput.next();
-                    k = cityInput.nextInt();
-                    System.out.println();
-                    continue;
-                }
-            }
-            catch(InputMismatchException e)
-            {
+        do {
+            while (!cityInput.hasNextInt()) {
+                String input = cityInput.next();
                 System.out.println("Must be an integer between 4 and 9");
-                k = cityInput.nextInt();
-                System.out.println();
-                continue;
             }
-        }
-        System.out.println("Enter the length of one side of the square grid between 10 and 30:");            
-        n = cityInput.nextInt();
-        while(true)
-        {
-            try
-            {
-                if(n >= 10 && n <= 30)
-                    break;
-                else
-                {
-                    System.out.println("That is an integer, but it must be between 10 and 30");
-                    n = cityInput.nextInt();
-                    System.out.println();
-                    continue;
-                }
-            }
-            catch(InputMismatchException e)
-            {
+            if (k < 4 || k > 9)
+                System.out.println("Must be an integer between 4 and 9");
+            k = cityInput.nextInt();
+        } while (k < 4 || k > 9);
+
+         System.out.println("Enter the length of one side of the square grid between 10 and 30:");            
+
+         do {
+            while (!cityInput.hasNextInt()) {
+                String input = cityInput.next();
                 System.out.println("Must be an integer between 10 and 30");
-                n = cityInput.nextInt();
-                System.out.println();
-                continue;
             }
-        }
+            if (n < 10 || n > 30 )
+                System.out.println("Must be an integer between 10 and 30");
+            n = cityInput.nextInt();
+        } while (n < 10 || n > 30);
+
         cityInput.close();
         System.out.println(k + " cities");
         System.out.println(n + " length");
