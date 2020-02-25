6d5d72c6a396b7288bb9cea88de39cd0967a7b48
mexican-wolf-trance <bargain.ghost@yandex.com>
2020-02-24 19:25:50 -0600

    Added comments

--- randomCityPathGenerator/src/PathFinder.java
+++ randomCityPathGenerator/src/PathFinder.java
@@ -9,15 +9,20 @@ public class PathFinder
         int n = 0;
         final Scanner cityInput = new Scanner(System.in);
 
+        //Input validation time!!
         System.out.println("Enter the number of cities between 4 and 9:");            
 
         do 
         {
+            //Check if the user entered a string and then throw an error
+            //If it is a string, try again
             while (!cityInput.hasNextInt()) 
             {
                 String input = cityInput.next();
                 System.out.println("Must be an integer between 4 and 9");
             }
+            //Check if the integer the user finally entered is within the parameters
+            //Try again if not
             k = cityInput.nextInt();
             if (k < 4 || k > 9)
             {
@@ -29,6 +34,7 @@ public class PathFinder
 
          do 
          {
+            //Same as above except for the grid dimension
             while (!cityInput.hasNextInt()) 
             {
                 String input = cityInput.next();
