8cbc0c797083c7404915163c2e818443402d526d
jwlq89 <jwlq89@mail.umsl.edu>
2020-02-24 19:25:11 -0600

    Basic City Comments

--- randomCityPathGenerator/src/City.java
+++ randomCityPathGenerator/src/City.java
@@ -3,22 +3,24 @@ package src;
 import java.lang.Math;
 
 public class City {
-    private int x;
-    private int y;
+    private int x; // X-coordinate
+    private int y; // Y-coordinate
 
+    /* Constructor */
     City( int inX, int inY ) {
         this.x = inX;
         this.y = inY;
     }
 
+    /* Getters */
     public int getX() {
         return this.x;
     }
-
     public int getY() {
         return this.y;
     }
 
+    /* Calculate distance between two cities using the Distance Formula */
     public double distance( City city ) {
         return Math.pow(Math.pow(city.getX() - this.x, 2) + Math.pow(city.getY() - this.y, 2), .5 );
     }
