5e20256465942a37db281636b387852a4ae2b6a9
Jared Hunsaker <jth157@aol.com>
2020-02-27 00:39:32 -0600

    added comments to explain code a bit

--- randomCityPathGenerator/src/Main.java
+++ randomCityPathGenerator/src/Main.java
@@ -18,23 +18,29 @@ public class Main
 
         boolean end = false;
 		System.out.println("Hello World");
-		int[] nums = new int[]{0,1,2,3,4,5};
+		int[] nums = new int[]{0,1,2,3,4,5}; //temporary hardcoded city list
 		
 		int count = 1;
-		int max = numPermutations(5);
+        int max = numPermutations(5); //max set to 1 less than size because they all start at 0, will end up being K - 1
+        //test output
 		for (int i = 0; i < nums.length; i++){
 	        System.out.print(nums[i]);
 	    }
-	    System.out.println();
+        System.out.println();
+
+        //runs through the following code for every iteration of the order of cities
+        //checks against both the expected number of iterations and if it reaches the actual last iteration, just in case
 		while (count < max && end == false){
-		    end = nextOrder(nums, end);
-		    for (int i = 0; i < nums.length; i++){
+		    end = nextOrder(nums, end);               //returns true if the last iteration is found, auto updates order since arrays are passed by ref
+		    for (int i = 0; i < nums.length; i++){    //more testing output
 		        System.out.print(nums[i]);
-		    }
+            }
+            //*call to calculate distance of path based on order here
+            //*call to compare against lowest found so far here
 		    System.out.println();
 		    count++;
 		}
-		System.out.println("Permutations: " + count);
+		System.out.println("Permutations: " + count); //more testing output
 
     }
     public static boolean nextOrder(int[] order, boolean end){
@@ -57,7 +63,10 @@ public class Main
 	        swap(order, I, J);
 	        reverse(order, I+1);
 	    }
-	    boolean true_after_swap = false;
+        boolean true_after_swap = false;
+        
+        //reruns the first check to see if it becomes the last iteration after the above changes
+        //avoids the function being called twice for the last iteration, may clean up to avoid needed this
 	    for (int i = 1; i < order.length - 1; i++){
 	        if (order[i] < order[i+1]){
 	            true_after_swap = true;
@@ -69,14 +78,15 @@ public class Main
 	    
 	    return end;
 	}
-	
+    
+    //simple swap function to switch two values in an array
 	public static void swap(int[] order, int I, int J){
-	    //System.out.println("I: " + I + "   J:" + J);
 	    int temp = order[I];
 	    order[I] = order[J];
 	    order[J] = temp;
 	}
-	
+    
+    //function to reverse the end of an array starting at a given index
 	public static void reverse(int[] order, int I){
 	    int j = order.length;
 	    int[] rev_order = new int[order.length];
@@ -91,7 +101,8 @@ public class Main
 	        order[i] = rev_order[i];
 	    }
 	}
-	
+    
+    //factorial algorithm to return the expected amount of iterations for a given number of cities
 	public static int numPermutations(int k){
         if (k == 1)
             return 1;
