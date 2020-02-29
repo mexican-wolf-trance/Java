a273ecb0dedb7dd86a469e4804a3a1f3f87f67ed
jwlq89 <jwlq89@mail.umsl.edu>
2020-02-28 20:01:58 -0600

    Main Cleaned

--- randomCityPathGenerator/src/Main.java
+++ randomCityPathGenerator/src/Main.java
@@ -32,39 +32,12 @@ public class Main
         System.out.println();
 
         PathFinder pathfinder = new PathFinder(k,n);
+        pathfinder.calcDistances();
 
         pathfinder.outputGrid();
-        pathfinder.calcDistances();
         pathfinder.outputDistances();
 
-
-        boolean end = false;
-		Order order = new Order(k);
-		int[] cur_order = order.getOrder();
-
-		int count = 1;
-        int max = numPermutations(k); //max set to 1 less than size because they all start at 0, will end up being K - 1
-        //test output
-        System.out.println("\nDifferent city orders:");
-		for (int i = 0; i < k; i++){
-	        System.out.print(cur_order[i]);
-	    }
-        System.out.println();
-
-        //runs through the following code for every iteration of the order of cities
-        //checks against both the expected number of iterations and if it reaches the actual last iteration, just in case
-		while (count < max && end == false){
-		    end = order.nextOrder(cur_order, end);               //returns true if the last iteration is found, auto updates order since arrays are passed by ref
-		    for (int i = 0; i < cur_order.length; i++){    //more testing output
-		        System.out.print(cur_order[i]);
-            }
-            //*call to calculate distance of path based on order here
-            //*call to compare against lowest found so far here
-		    System.out.println();
-		    count++;
-		}
-		System.out.println("Permutations: " + count); //more testing output
-
+        pathfinder.outputPathways();
 	}
 
 	public static int inputInteger(Scanner input, int min, int max) {
@@ -92,13 +65,5 @@ public class Main
         } while (intInput < min || intInput > max);
 
         return intInput;
-    }
-
-    //factorial algorithm to return the expected amount of iterations for a given number of cities
-	public static int numPermutations(int k){
-        if (k == 1)
-            return 1;
-        else
-            return k * numPermutations(k-1);
     }
 }
